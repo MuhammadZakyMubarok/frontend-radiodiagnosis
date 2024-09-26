10c7e0cf4dcd0b20318d539327c4a80c2d6d9d9a
ardian_hermawan <imanuddin.ardian@gmail.com>
2024-09-26 15:16:51 +0700

    Refactor: Change route from '/patients/Patient' to '/Patient' (matching satu sehat)

--- src/api/satu-sehat/patient.service.js
+++ src/api/satu-sehat/patient.service.js
@@ -2,9 +2,9 @@ import { axiosSatuSehat } from "../../utils/axios";
 
 export default () => ({
     searchNIK (params) {
-        return axiosSatuSehat.get('patients/Patient', params);
+        return axiosSatuSehat.get('Patient', params);
     },
     searchById (id) {
-        return axiosSatuSehat.get('patients/Patient/' + id);
+        return axiosSatuSehat.get('Patient/' + id);
     }
 })
