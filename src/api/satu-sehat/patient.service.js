2a8e1cc49bc28168b41d9d3327f0aa205d033601
ardian_hermawan <imanuddin.ardian@gmail.com>
2024-10-15 14:10:29 +0700

    Feat: Add new patient API in service

--- src/api/satu-sehat/patient.service.js
+++ src/api/satu-sehat/patient.service.js
@@ -2,9 +2,42 @@ import { axiosSatuSehat } from "../../utils/axios";
 
 export default () => ({
     searchNIK (params) {
-        return axiosSatuSehat.get('Patient', params);
+        const { nik } = params;
+        return axiosSatuSehat.get('/Patient/by-nik', {
+            params: {
+                nik
+            }
+        });
+    },
+    searchByNIKIbu(params) {
+      const { nik } = params;
+      return axiosSatuSehat.get('/Patient/by-nik-ibu', {
+          params: {
+              nik
+          }
+      })
+    },
+    searchByNameBirthdateIdentifier(params) {
+      const { name, birthdate, identifier } = params;
+      return axiosSatuSehat.get('/Patient/by-name-birthdate-identifier', {
+          params: {
+              name,
+              birthdate,
+              identifier
+          }
+      })
+    },
+    searchByNameBirthdateGender(params) {
+        const { name, birthdate, gender } = params;
+        return axiosSatuSehat.get('/Patient/by-name-birthdate-gender', {
+            params: {
+                name,
+                birthdate,
+                gender
+            }
+        })
     },
     searchById (id) {
-        return axiosSatuSehat.get('Patient/' + id);
+        return axiosSatuSehat.get('/Patient/' + id);
     }
 })
