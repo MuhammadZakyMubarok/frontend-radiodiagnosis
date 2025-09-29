99df06d305f25930f1da50363daeb51892157227
ardian <imanuddin.ardian@gmail.com>
2025-09-29 13:52:20 +0700

    Make change to the Config.js to use backend.radiodiagnosis.id

--- src/routes/Config.js
+++ src/routes/Config.js
@@ -1,8 +1,8 @@
-export const baseURL = "http://localhost:5001"
+export const baseURL = "http://backend.radiodiagnosis.id:5001"
 export const apiUrl = "http://localhost:5010"
 //export const apiUrl = "https://952610eb0641.ngrok-free.app"
-export const satuSehatBaseURL = "http://localhost:5001/satu-sehat"
-export const satuSehatAuthURL = "http://localhost:5001/satu-sehat"
+export const satuSehatBaseURL = "http://backend.radiodiagnosis.id:5001/satu-sehat"
+export const satuSehatAuthURL = "http://backend.radiodiagnosis.id:5001/satu-sehat"
 
 
 // export const baseURL = "http://api.radiodiagnosis-report.me"
