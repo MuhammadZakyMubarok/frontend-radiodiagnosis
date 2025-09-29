fed04a35bf6ac153194fd0f2cc603b42b425e180
ardian <imanuddin.ardian@gmail.com>
2025-09-29 14:01:36 +0700

    Change the Config.js to use only the URL and HTTPS

--- src/routes/Config.js
+++ src/routes/Config.js
@@ -1,8 +1,8 @@
-export const baseURL = "http://backend.radiodiagnosis.id:5001"
-export const apiUrl = "http://localhost:5010"
+export const baseURL = "https://backend.radiodiagnosis.id"
+export const apiUrl = "https://localhost:5010"
 //export const apiUrl = "https://952610eb0641.ngrok-free.app"
-export const satuSehatBaseURL = "http://backend.radiodiagnosis.id:5001/satu-sehat"
-export const satuSehatAuthURL = "http://backend.radiodiagnosis.id:5001/satu-sehat"
+export const satuSehatBaseURL = "https://backend.radiodiagnosis.id/satu-sehat"
+export const satuSehatAuthURL = "https://backend.radiodiagnosis.id/satu-sehat"
 
 
 // export const baseURL = "http://api.radiodiagnosis-report.me"
